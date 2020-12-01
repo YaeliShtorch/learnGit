5d9d836bde1689383e3c3d9c06780376a0345c41
PninaM <45176849+PninaM@users.noreply.github.com>
2020-12-01 19:59:43 +0200

    statistics

--- API/Controllers/StatisticsController.cs
+++ API/Controllers/StatisticsController.cs
@@ -6,6 +6,7 @@ using System.Web.Http;
 using System.Web.Http.Cors;
 using DAL;
 using BL;
+using System.Dynamic;
 
 namespace API.Controllers
 {
@@ -14,5 +15,47 @@ namespace API.Controllers
     [RoutePrefix("api/Statistics")]
     public class StatisticsController
     {
+        StatisticsLogic ST = new StatisticsLogic();
+
+        //get order percentage by company
+        [Route("GetOrderPCTByComp")]
+        [HttpGet]
+        public Dictionary<string, double> OrderPercentByCompany(DateTime date)
+        {
+                return ST.getOrderPercentByProvider(date);
+        }
+
+        //get average days of companies sending items
+        [Route("GetAVGDayDelByComp")]
+        [HttpGet]
+        public Dictionary<string, double> avgMaterialProviding(DateTime date)
+        {
+            return ST.avgMaterialProviding(date);
+        }
+
+        //get amount and PCT of new customers by given date
+        [Route("GetCountNewCustomers")]
+        [HttpGet]
+        public ExpandoObject customerCount(DateTime date)
+        {
+            return ST.customerCount(date);
+        }
+
+        //get amount and PCT of new orders by given date
+        [Route("GetCountNewOrders")]
+        [HttpGet]
+        public ExpandoObject orderCount(DateTime date)
+        {
+            return ST.orderCount(date);
+        }
+
+        //get amount and PCT for each materials ordered
+        [Route("GetCountOrderedMaterials")]
+        [HttpGet]
+        public Dictionary<string, ExpandoObject> MaterialsQuantityOrder(DateTime date)
+        {
+            return ST.MaterialsQuantityOrder(date);
+        }
+
     }
 }
--- BL/StatisticsLogic.cs
+++ BL/StatisticsLogic.cs
@@ -5,25 +5,27 @@ using System.Text;
 using System.Threading.Tasks;
 using MathNet.Numerics.Statistics;
 using DAL;
+using System.Dynamic;
+
 namespace BL
 {
-    class StatisticsLogic : BaseLogic
+    public class StatisticsLogic : BaseLogic
     {
         //returns name of companies and their order percentage from a certain date to current date
         public Dictionary<String, double> getOrderPercentByProvider(DateTime date)
         {
             Dictionary<String, double> results = new Dictionary<string, double>();
             string company;
-            double count=0;
+            double count = 0;
             var orderP = db.MaterialProviders.Where(i => i.datePApproved >= date).GroupBy(p => p.ProviderId).ToList();
 
             if (orderP != null)
             {
                 //for each key-Provider
-                orderP.ToList().ForEach(pID=>
+                orderP.ToList().ForEach(pID =>
                 {
                     //percentage
-                    count = 100*pID.Count()/orderP.Count();
+                    count = 100 * pID.Count() / orderP.Count();
                     company = db.Providers.FirstOrDefault(p => p.Id == pID.Key).CompanyName;
                     results.Add(company, count);
                     count = 0;
@@ -40,7 +42,7 @@ namespace BL
         {
 
             Dictionary<String, double> results = new Dictionary<string, double>();
-            double avgDay=0;
+            double avgDay = 0;
             string company;
             var orderP = db.MaterialProviders.Where(i => i.datePApproved >= date).GroupBy(p => p.ProviderId).ToList();
             if (orderP != null)
@@ -70,54 +72,88 @@ namespace BL
         }
 
         //returns # of new customers and it's percentage from a certain date 
-        public List<double> customerCount(DateTime date)
+        public ExpandoObject customerCount(DateTime date)
         {
+            dynamic result = new ExpandoObject();
             double cNum;
             double totalCust;
             double percentage;
-            List<double> custNum=new List<double>();
             var customers = db.Customers.Where(d => d.registeredDate >= date);
             totalCust = db.Customers.Count();
             cNum = customers.Count();
             percentage = 100 * cNum / totalCust;
             //number of new customers
-            custNum.Add(cNum);
+            result.custNum = cNum;
             //percentage of new customers
-            custNum.Add(percentage);
-            return custNum;
+            result.custPercent = percentage;
+            return result;
         }
 
-        public List<double> orderCount(DateTime date)
+        public ExpandoObject orderCount(DateTime date)
         {
+            dynamic result = new ExpandoObject();
             double oNum;
             double percentage;
             double totalOrders;
-            List<double> orderNum = new List<double>();
             var orders = db.Orders.Where(d => d.OrderDate >= date);
             totalOrders = db.Orders.Count();
             oNum = orders.Count();
-            percentage = 100 * oNum /totalOrders;
+            percentage = 100 * oNum / totalOrders;
             //number of new customers
-            orderNum.Add(oNum);
+            result.orderNum = oNum;
             //percentage of new customers
-            orderNum.Add(percentage);
-            return orderNum;
+            result.orderPercent = percentage;
+            return result;
         }
 
         //returns for each material name quantities ordered and percentage
-        public Dictionary<string, List<double>> MaterialsQuantityOrder(DateTime date)
+        public Dictionary<string, ExpandoObject> MaterialsQuantityOrder(DateTime date)
         {
-            Dictionary<string, List<double>> results = new Dictionary<string, List<double>>();
-            string materialName;
-            double quantity;
-            double percentage;
-            var orders = db.Orders.Where(o => o.OrderDate >= date);
-            orders.ToList().ForEach(o => {
-                var materials = db.MaterialTypeOrders.Where(m => m.OrderId == o.Id);
+            Dictionary<string, ExpandoObject> results = new Dictionary<string, ExpandoObject>();
+            dynamic resNum = new ExpandoObject();
+            string materialName="";
+            double quantity = 0;
+            double totalOrdered = 0;
+            bool IsMat = false;
+            List<Order> orders = new List<Order>();
+            List<MaterialTypeOrder> materials = new List<MaterialTypeOrder>();
+
+            var materialNames = db.Materials.ToList();
+
+            orders = db.Orders.Where(o => o.OrderDate >= date).ToList();
+
+            orders.ToList().ForEach(o =>
+            {
+                materials = db.MaterialTypeOrders.Where(m => m.OrderId == o.Id).ToList();
+            });
+            //sum of all items ordered
+            materials.ForEach(m => { totalOrdered += m.Amount; });
+            //looping through material names tbl
+            materialNames.ForEach(m =>
+            {
+                //looping through materials ordered
+                materials.ForEach(mo =>
+                {
+                    if (mo.MaterialId == m.Id)
+                    { 
+                        materialName = m.Name;
+                    quantity += mo.Amount;
+                        IsMat = true;
+                    }
+                });
+
+                if (IsMat == true) { 
+                resNum.matQuantity=quantity ;
+                resNum.matPercent = 100 * quantity / totalOrdered;
+                results.Add(materialName, resNum);
+                }
+                quantity = 0;
+                resNum.matmatQuantity = 0;
+                resNum.matPercent = 0;
+
+
             });
             
-            //materials.
-
             return results;
         }
     }
